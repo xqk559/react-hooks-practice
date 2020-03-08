0abed3cc0d368cddb0cebf1b94afc34ef55c6a55
keith <xqk559@gmail.com>
2020-03-08 09:52:59 -0400

    Set clearTimeout so only one timer runs at once

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo(props => {
   const inputRef = useRef();
 
   useEffect(()=>{
-    setTimeout(()=>{
+    const timer = setTimeout(()=>{
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
         fetch('https://react-hooks-practice-3f0b3.firebaseio.com/ingredients.json' + query)
@@ -27,6 +27,9 @@ const Search = React.memo(props => {
         });
       }
     }, 500);
+    return () => {
+      clearTimeout(timer);
+    }
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
