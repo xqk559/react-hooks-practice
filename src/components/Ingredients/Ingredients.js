c3e07d295af6252b01d31bfd19e6e75dac3d55dd
keith <xqk559@gmail.com>
2020-03-08 07:58:07 -0400

    Passed addIngredientHandler as a prop to handle submit button clicks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,25 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
 function Ingredients() {
+const [userIngredients, setUserIngredients] = useState([]);
+
+const addIngredientHandler = ingredient => {
+  setUserIngredients(prevIngredients => [
+    ...prevIngredients, 
+    {id: Math.random().toString(), 
+    ...ingredient}]);
+};
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={()=>{}}/>
       </section>
     </div>
   );
