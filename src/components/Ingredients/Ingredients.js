955fe61ab898288165a5fbef9b198fabf40aa17d
keith <xqk559@gmail.com>
2020-03-08 08:28:34 -0400

    Used array filter to remove list elements by id

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -13,13 +13,17 @@ const addIngredientHandler = ingredient => {
     ...ingredient}]);
 };
 
+const removeIngredientHandler = ingredientId => {
+  setUserIngredients(prevIngredients => prevIngredients.filter((ingredient)=> ingredient.id !== ingredientId));
+}
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={()=>{}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
