d47831ad818287b4a4822025de56ee0316296742
keith <xqk559@gmail.com>
2020-03-08 07:35:13 -0400

    USed array destructuring to make useState() code more clear

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''});
+  const [inputState, setInputState] = useState({title: '', amount: ''});
 
   const submitHandler = event => {
     event.preventDefault();
@@ -19,23 +19,23 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             <input type="text" 
                    id="title" 
-                   value={inputState[0].title} 
+                   value={inputState.title} 
                    onChange={event=>{
                      const newTitle = event.target.value;
-                    inputState[1](prevInputState=>({
+                    setInputState(prevInputState=>({
                      title: newTitle, 
-                     amount: inputState[0].amount}))}}/>
+                     amount: inputState.amount}))}}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" 
                    id="amount" 
-                   value={inputState[0].amount} 
+                   value={inputState.amount} 
                    onChange={event=>{
                     const newAmount = event.target.value;
-                    inputState[1](prevInputState=>({
+                    setInputState(prevInputState=>({
                       amount: newAmount, 
-                      title: inputState[0].title}))}}/>
+                      title: inputState.title}))}}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
